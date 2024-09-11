2aa21be2f503efeef543f6db041258a43be2bfbe
unknown <sigwaldos@gmail.com>
2024-09-11 22:28:25 +0700

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { Inter } from "next/font/google"
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter"
+})
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
