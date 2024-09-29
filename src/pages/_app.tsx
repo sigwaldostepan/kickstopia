4a8677ea5bab1090be10be8425d134fbbef0c3f5
unknown <sigwaldos@gmail.com>
2024-09-29 11:48:30 +0700

    feat: add homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google"
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
@@ -9,8 +9,8 @@ import "~/styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-inter"
-})
+  variable: "--font-inter",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
